e1b1bbd7208729f1141f52609f8f76ade937762c
gyeongbibin <151108886+gyeongbibin@users.noreply.github.com>
2025-11-10 17:53:52 +0900

    Tailwind 설치 & 디렉토리 초기  세팅

---
+++ Week3/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-red-500 text-white text-2xl p-4">Tailwind 테스트 ✅</div>
+  );
+}
+
+export default App;
