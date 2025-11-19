f2381fe2011c912944abe42537ec7cb42156bdf4
gyeongbibin <151108886+gyeongbibin@users.noreply.github.com>
2025-11-20 00:40:03 +0900

    feat: 마이페이지 스타일 및 라우팅 설정

--- Week4/src/pages/my-page/MyPage.tsx
+++ Week4/src/pages/my-page/MyPage.tsx
@@ -1,4 +1,19 @@
-export const MyPage = () => {
-  return <div>마이페이지</div>;
-};
+import { Routes, Route } from 'react-router-dom';
+import { Header } from '../../widgets/header';
+import { MyInfoPage } from './MyInfoPage';
+import { UserLookupPage } from './UserLookupPage';
+import { UserDeletePage } from './UserDeletePage';
+import { myPageContainerStyle } from './MyPage.css.ts';
 
+export const MyPage = () => {
+  return (
+    <div className={myPageContainerStyle}>
+      <Header />
+      <Routes>
+        <Route index element={<MyInfoPage />} />
+        <Route path="lookup" element={<UserLookupPage />} />
+        <Route path="delete" element={<UserDeletePage />} />
+      </Routes>
+    </div>
+  );
+};
